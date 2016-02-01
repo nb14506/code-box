789deb3934c15b215ecf47e507e3590de1b6d8e1
nb14506 <nb14506@shibaura-it.ac.jp>
2016-02-01 19:23:12 +0900

    First created

---
+++ SourceDemoGit/SourceDemoGit.cpp
@@ -0,0 +1,12 @@
+// SourceDemoGit.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello";
+	return 0;
+}
+
